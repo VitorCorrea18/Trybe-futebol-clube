573152a74eac33cdcc65ef99d10e94841cc5b805
Vitor Correa <vitorcorrea18@gmail.com>
2022-07-07 17:59:38 -0400

    feat: create User migration and model, also remove underline on user seeder file name

---
+++ app/backend/src/database/models/User.ts
@@ -0,0 +1,42 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+
+class User extends Model {
+  public id: number;
+  public username: string;
+  public role: string;
+  public email: string;
+  public password: string;
+}
+
+User.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER,
+  },
+  username: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+  role: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+  email: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+  password: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'user',
+  timestamps: false,
+});
+
+export default User;

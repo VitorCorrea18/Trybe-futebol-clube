8ede1237e735e2e6abdb4f8b2098a2698c80e06a
Vitor Correa <vitorcorrea18@gmail.com>
2022-07-11 17:59:09 -0400

    fix: removing token schema

--- app/backend/src/schemas/index.ts
+++ app/backend/src/schemas/index.ts
@@ -1,2 +1,2 @@
 export { default as loginSchema } from './loginSchema';
-export { default as tokenSchema } from './tokenSchema';
+export default {}; // lint
--- app/backend/src/schemas/tokenSchema.ts
+++
@@ -1,7 +0,0 @@
-import * as Joi from 'joi';
-
-const tokenSchema = Joi.object({
-  token: Joi.string().required(),
-});
-
-export default tokenSchema;

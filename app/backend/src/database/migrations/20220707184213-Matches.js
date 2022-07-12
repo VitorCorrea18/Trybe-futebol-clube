1686b44d7edb7aefd89f139dd3249a5c069d1583
Vitor Correa <vitorcorrea18@gmail.com>
2022-07-11 20:19:14 -0400

    feat: create Matches migration

---
+++ app/backend/src/database/migrations/20220707184213-Matches.js
@@ -0,0 +1,58 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+     await queryInterface.createTable('matches', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      homeTeam: {
+        allowNull: false,
+        field: 'home_team',
+        type: Sequelize.INTEGER,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        primaryKey: true,
+        references: {
+          model: 'teams',
+          key: 'id'
+        },
+      },
+      homeTeamGoals: {
+        allowNull: false,
+        field: 'home_team_goals',
+        type: Sequelize.INTEGER,
+      },
+      awayTeam: {
+        allowNull: false,
+        field: 'away_team',
+        type: Sequelize.INTEGER,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        primaryKey: true,
+        references: {
+          model: 'teams',
+          key: 'id'
+        },
+      },
+      awayTeamGoals: {
+        allowNull: false,
+        field: 'away_team_goals',
+        type: Sequelize.INTEGER
+      },
+      inProgress: {
+        allowNul: false,
+        field: 'in_progress',
+        type: Sequelize.INTEGER,
+        defaultValue: '0'
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('matches');
+  }
+};

fded53b7df2828e440e6428a79eee9e1a6711f63
Vitor Correa <vitorcorrea18@gmail.com>
2022-07-17 15:35:08 -0400

    feat: matches model and migration

---
+++ app/backend/src/database/models/MatchModel.ts
@@ -0,0 +1,57 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+import Team from './TeamModel';
+
+class MatchModel extends Model {
+  id: number;
+  homeTeam:number;
+  homeTeamGoals:number;
+  awayTeam:number;
+  awayTeamGoals:number;
+  inProgress:boolean;
+}
+
+MatchModel.init(
+  {
+    id: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    homeTeam: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+    },
+    homeTeamGoals: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+    },
+    awayTeam: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+    },
+    awayTeamGoals: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+    },
+    inProgress: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+    },
+  },
+  {
+    underscored: true,
+    sequelize: db,
+    modelName: 'matchModel',
+    timestamps: false,
+  },
+);
+
+Team.hasMany(MatchModel, { foreignKey: 'homeTeam', as: 'home_team' });
+Team.hasMany(MatchModel, { foreignKey: 'awayTeam', as: 'away_team' });
+
+MatchModel.belongsTo(Team, { foreignKey: 'homeTeam', as: 'home_team' });
+MatchModel.belongsTo(Team, { foreignKey: 'awayTeam', as: 'away_team' });
+
+export default MatchModel;

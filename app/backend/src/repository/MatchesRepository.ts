cf6269b45060704a9cde8526763bc5985b548cdc
Vitor Correa <vitorcorrea18@gmail.com>
2022-07-18 20:59:05 -0300

    fix: repository can be used by diferent functions from service

--- app/backend/src/repository/MatchesRepository.ts
+++ app/backend/src/repository/MatchesRepository.ts
@@ -1,23 +1,30 @@
 import MatchModel from '../database/models/MatchModel';
 import { IMatch, IMatchModel } from '../protocols';
 
+type data = { where: object };
+
 export default class MatchesRepository implements IMatchModel {
   constructor(private model = MatchModel) {
     this.model = model;
   }
 
-  async findAll(): Promise<IMatch[]> {
-    const result = await this.model.findAll();
+  async findAll(data: object): Promise<IMatch[]> {
+    const result = await this.model.findAll(data);
     return result as IMatch[];
   }
 
-  async findInProgress(inProgress: boolean): Promise<IMatch[]> {
-    const result = await this.model.findAll({ where: { inProgress } });
+  async findAllInProgress(data: object): Promise<IMatch[]> {
+    const result = await this.model.findAll(data);
     return result as IMatch[];
   }
 
-  async findOne(id: number): Promise<IMatch> {
-    const result = await this.model.findOne({ where: { id } });
-    return result as IMatch;
+  async create(payload:object): Promise<IMatch> {
+    const result = await this.model.create({ ...payload, inProgress: true });
+    return result;
+  }
+
+  async update(payload: object, data: data): Promise<number> {
+    const [result] = await this.model.update(payload, data);
+    return result;
   }
 }

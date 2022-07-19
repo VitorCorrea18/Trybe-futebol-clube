5bb93e0de9773a5b405376da3881167668c198b2
Vitor Correa <vitorcorrea18@gmail.com>
2022-07-19 20:29:23 -0300

    feat: update massages

--- app/backend/src/utils/messages.ts
+++ app/backend/src/utils/messages.ts
@@ -2,10 +2,12 @@ const messages = {
   internalError: 'Internal server error',
   missingField: 'All fields must be filled',
   failedLogin: 'Incorrect email or password',
-  invalidToken: 'Invalid Token',
+  invalidToken: 'Token must be a valid token',
   notFound: 'Not Found',
   invalidId: 'Invalid ID',
   finished: 'Finished',
+  equalTeams: 'It is not possible to create a match with two equal teams',
+  invalidTeam: 'There is no team with such id!',
 };
 
 export default messages;

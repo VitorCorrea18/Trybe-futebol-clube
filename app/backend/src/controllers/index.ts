3b6e7b6279d7a1c0c39961da89dcf75f9c8d335d
Vitor Correa <vitorcorrea18@gmail.com>
2022-07-18 21:02:34 -0300

    feat: matches controller index

--- app/backend/src/controllers/index.ts
+++ app/backend/src/controllers/index.ts
@@ -1,3 +1,3 @@
 export { default as UserController } from './UserController';
 export { default as TeamController } from './TeamController';
-export { default as MatchesController } from './MatchesConstroller';
+export { default as MatchesController } from './MatchesController';

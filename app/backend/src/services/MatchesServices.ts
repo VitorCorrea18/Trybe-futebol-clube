9ad806e669b86b0b5cd1b3b6c04416d2f52404f0
Vitor Correa <vitorcorrea18@gmail.com>
2022-07-18 21:00:06 -0300

    feat: create matches services

---
+++ app/backend/src/services/MatchesServices.ts
@@ -0,0 +1,49 @@
+import { IMatch, IMatchModel, IMatchService } from '../protocols';
+import TeamModel from '../database/models/TeamModel';
+import { CustomError, httpStatus, messages } from '../utils';
+
+export default class MatchesServices implements IMatchService {
+  constructor(private model: IMatchModel) {
+    this.model = model;
+  }
+
+  async getAll(): Promise<IMatch[]> {
+    const result = await this.model.findAll({
+      include: [
+        { model: TeamModel, as: 'homeTeam', attributes: { exclude: ['id'] } },
+        { model: TeamModel, as: 'awayTeam', attributes: { exclude: ['id'] } },
+      ],
+    });
+    console.log('service matches', result);
+    return result as IMatch[];
+  }
+
+  async getInProgress(inProgress: string): Promise<IMatch[]> {
+    const result = await this.model.findAll({
+      where: { inProgress },
+      include: [
+        { model: TeamModel, as: 'homeTeam', attributes: { exclude: ['id'] } },
+        { model: TeamModel, as: 'awayTeam', attributes: { exclude: ['id'] } },
+      ],
+    });
+    return result as IMatch[];
+  }
+
+  async create(payload: object): Promise<IMatch> {
+    const result = await this.model.create(payload);
+    return result as IMatch;
+  }
+
+  async updateGoals(id: number, matchGoals: object): Promise<number> {
+    const result = await this.model.update(matchGoals, { where: { id } });
+    if (!result) {
+      throw new CustomError(httpStatus.notFound, messages.notFound);
+    }
+    return result;
+  }
+
+  async finishMatch(id: number): Promise<number> {
+    const result = await this.model.update({ inProgress: false }, { where: { id } });
+    return result;
+  }
+}

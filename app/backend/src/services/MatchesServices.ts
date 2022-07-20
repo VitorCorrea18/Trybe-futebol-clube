b2658930852f1d4cfbe307646897e69300176fb8
Vitor Correa <vitorcorrea18@gmail.com>
2022-07-19 22:26:38 -0300

    fix: MatchesServices - update getByProgress, validation on create and updateGoals return void

--- app/backend/src/services/MatchesServices.ts
+++ app/backend/src/services/MatchesServices.ts
@@ -1,10 +1,11 @@
-import { IMatch, IMatchModel, IMatchService } from '../protocols';
+import { IMatch, IMatchModel, IMatchService, ITeamModel } from '../protocols';
 import TeamModel from '../database/models/TeamModel';
 import { CustomError, httpStatus, messages } from '../utils';
 
 export default class MatchesServices implements IMatchService {
-  constructor(private model: IMatchModel) {
+  constructor(private model: IMatchModel, private teamModel: ITeamModel) {
     this.model = model;
+    this.teamModel = teamModel;
   }
 
   async getAll(): Promise<IMatch[]> {
@@ -17,9 +18,10 @@ export default class MatchesServices implements IMatchService {
     return result as IMatch[];
   }
 
-  async getInProgress(inProgress: string): Promise<IMatch[]> {
+  async getByProgress(inProgress: string): Promise<IMatch[]> {
+    console.log(inProgress);
     const result = await this.model.findAll({
-      where: { inProgress },
+      where: { inProgress: inProgress === 'true' },
       include: [
         { model: TeamModel, as: 'teamHome', attributes: { exclude: ['id'] } },
         { model: TeamModel, as: 'teamAway', attributes: { exclude: ['id'] } },
@@ -28,17 +30,25 @@ export default class MatchesServices implements IMatchService {
     return result as IMatch[];
   }
 
-  async create(payload: object): Promise<IMatch> {
+  async create(payload: IMatch): Promise<IMatch> {
+    // validate teams
+    const homeTeam = await this.teamModel.findOne(payload.homeTeam);
+    const awayTeam = await this.teamModel.findOne(payload.awayTeam);
+    if (!homeTeam || !awayTeam) {
+      throw new CustomError(httpStatus.badRequest, messages.invalidTeam);
+    }
+    if (homeTeam.id === awayTeam.id) {
+      throw new CustomError(httpStatus.unauthorized, messages.equalTeams);
+    }
     const result = await this.model.create(payload);
     return result as IMatch;
   }
 
-  async updateGoals(id: number, matchGoals: object): Promise<number> {
+  async updateGoals(id: number, matchGoals: object): Promise<void> {
     const result = await this.model.update(matchGoals, { where: { id } });
     if (!result) {
       throw new CustomError(httpStatus.notFound, messages.notFound);
     }
-    return result;
   }
 
   async finishMatch(id: number): Promise<number> {
